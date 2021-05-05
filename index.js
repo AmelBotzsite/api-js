0e9a44078224ba288af098ffd485ae3cda893786
Caliph71 <74745394+Caliph71@users.noreply.github.com>
2021-05-05 13:57:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var morgan = require('morgan');
-var tiktok = require('./routes/main');
+var express = require('express'),
+    cors = require('cors'),
+    secure = require('ssl-express-www');
+const PORT = process.env.PORT || 8080 || 5000 || 3000
 
+var mainrouter = require('./routes/main');
 
-var app = express();
-const PORT = 80;
+var app = express()
+app.enable('trust proxy');
+app.set("json spaces",2)
+app.use(cors())
+app.use(secure)
+app.use(express.static("public"))
 
-app.use(morgan('dev'));
-app.use(express.static('public'));
-
-
-// Enable CORS on ExpressJS to avoid cross-origin errors when calling this server using AJAX
-// We are authorizing all domains to be able to manage information via AJAX (this is just for development)
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,recording-session");
-    next();
-});
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-
-app.set('json spaces',2)
-app.use('/', tiktok);
-
+app.use('/', mainrouter)
 
 app.listen(PORT, () => {
-    console.log(`Server Run on port ${PORT}`)
-});
+    console.log("Server running on port " + PORT)
+})
+
+module.exports = app
